a7bc665dc05f1893573254f31187e793b5ce72ef
no215tyler <no.215.tyler@gmail.com>
2024-08-10 23:56:01 +0900

    掛け算2: C#編

--- paiza/Program.cs
+++ paiza/Program.cs
@@ -4,8 +4,12 @@ class Program
 {
     static void Main()
     {
-        Greet greet = new Greet("NOB", "Japanese", "Hello, C#");
-        greet.Greeting();
+        string[] strNums = Console.ReadLine().Split(' ');
+        int[] nums = Array.ConvertAll(strNums, int.Parse);
+
+        int x = nums[0] * nums[0];
+        int y = (nums[1] * nums[1]) + (nums[2] * nums[2]);
+        Console.WriteLine($"{x} {y}");
     }
 }
 
