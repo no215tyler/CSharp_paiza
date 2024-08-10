6f253a04c76bcef654fbd14d373b9fa788e8e6be
no215tyler <no.215.tyler@gmail.com>
2024-08-11 00:21:10 +0900

    条件分岐_ゼロ以外: C#編

--- paiza/Program.cs
+++ paiza/Program.cs
@@ -4,12 +4,18 @@ class Program
 {
     static void Main()
     {
-        string[] strNums = Console.ReadLine().Split(' ');
-        int[] nums = Array.ConvertAll(strNums, int.Parse);
-        int n = 10000;
-        n = n / nums[0];
-        n = n % nums[1];
-        Console.WriteLine($"{n}");
+        //string[] strNums = Console.ReadLine().Split(' ');
+        //int[] nums = Array.ConvertAll(strNums, int.Parse);
+
+        int num = int.Parse(Console.ReadLine());
+        if (num != 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
     }
 }
 
