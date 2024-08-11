b4911a1196742cc968eea850c90462b85eec50aa
no215tyler <no.215.tyler@gmail.com>
2024-08-11 09:18:18 +0900

    累乗2(Math.Pow): C#編

--- paiza/Program.cs
+++ paiza/Program.cs
@@ -4,18 +4,11 @@ class Program
 {
     static void Main()
     {
-        //string[] strNums = Console.ReadLine().Split(' ');
-        //int[] nums = Array.ConvertAll(strNums, int.Parse);
+        string[] strNums = Console.ReadLine().Split(' ');
+        int[] nums = Array.ConvertAll(strNums, int.Parse);
 
-        int num = int.Parse(Console.ReadLine());
-        if (num != 0)
-        {
-            Console.WriteLine("YES");
-        }
-        else
-        {
-            Console.WriteLine("NO");
-        }
+        double result = Math.Pow(((nums[0] + nums[1]) * nums[2]), 2) % nums[3];
+        Console.WriteLine(result);
     }
 }
 
