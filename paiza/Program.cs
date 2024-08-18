ba763635e3eaed80fceb96abd8f829dc389650e2
no215tyler <no.215.tyler@gmail.com>
2024-08-19 00:00:33 +0900

    カンマ区切りのN個のデータの入力: C#編

--- paiza/Program.cs
+++ paiza/Program.cs
@@ -4,11 +4,14 @@ class Program
 {
     static void Main()
     {
-        string[] strNums = Console.ReadLine().Split(' ');
-        int[] nums = Array.ConvertAll(strNums, int.Parse);
+        int n = int.Parse(Console.ReadLine());
+        string[] strs = Console.ReadLine().Split(',');
+        //int[] nums = Array.ConvertAll(strNums, int.Parse);
 
-        double result = Math.Pow(((nums[0] + nums[1]) * nums[2]), 2) % nums[3];
-        Console.WriteLine(result);
+        foreach (string str in strs)
+        {
+            Console.WriteLine(str);
+        }
     }
 }
 
