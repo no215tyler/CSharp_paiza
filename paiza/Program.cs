ce1e427d5138796721e8bd14cc16b8b22306d2b1
no215tyler <no.215.tyler@gmail.com>
2024-08-23 16:52:48 +0900

    巨大な数のかけ算: C#編

--- paiza/Program.cs
+++ paiza/Program.cs
@@ -1,15 +1,15 @@
 ﻿namespace paiza;
+using System.Numerics;
 
 class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
-        string strNums = Console.ReadLine();
-        string[] numsArray = strNums.Split(' ');
-        int[] nums = Array.ConvertAll(numsArray, int.Parse);
-        int count = nums.Count(num => num == 1);
-        Console.WriteLine(count);
+        // int型ではなくlong型の型注釈を使用することでオーバーフローを免れる
+        // さらに大きい数値を扱う場合はBigInteger型を定義する（using System.Numericsの名前空間インポートが必要）
+        BigInteger num1 = BigInteger.Parse(Console.ReadLine());
+        BigInteger num2 = BigInteger.Parse(Console.ReadLine());
+        BigInteger result = num1 * num2;
+        Console.WriteLine(result);
     }
 }
-
