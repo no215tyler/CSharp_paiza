7612cf5a604ac246fc6fb55ebba18860bf8abf09
no215tyler <no.215.tyler@gmail.com>
2025-03-14 16:25:15 +0900

    配列基礎

--- paiza/Program.cs
+++ paiza/Program.cs
@@ -5,11 +5,12 @@ class Program
 {
     static void Main()
     {
-        // int型ではなくlong型の型注釈を使用することでオーバーフローを免れる
-        // さらに大きい数値を扱う場合はBigInteger型を定義する（using System.Numericsの名前空間インポートが必要）
-        BigInteger num1 = BigInteger.Parse(Console.ReadLine());
-        BigInteger num2 = BigInteger.Parse(Console.ReadLine());
-        BigInteger result = num1 * num2;
-        Console.WriteLine(result);
+        // 配列基礎
+        string input = Console.ReadLine();
+        string[] strAry = input.Split(" ");
+        foreach (string str in strAry)
+        {
+            Console.WriteLine($"{str}");
+        }
     }
 }
