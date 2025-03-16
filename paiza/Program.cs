c37798ac00f29896b4ad8004f1373448b3e0fc19
no215tyler <no.215.tyler@gmail.com>
2025-03-16 10:49:08 +0900

    乗客人数 C#編

--- paiza/Program.cs
+++ paiza/Program.cs
@@ -5,12 +5,28 @@ class Program
 {
     static void Main()
     {
-        // 引き算・掛け算 C#編
-        string[] nums = Console.ReadLine().Split(" ");
-        int num1 = int.Parse(nums[0]);
-        int num2 = int.Parse(nums[1]);
-        int result1 = num1 - num2;
-        int result2 = num1 * num2;
-        Console.WriteLine($"{result1} {result2}");
+        // 乗客人数 C#編
+        // ある電車に a 人が乗っています。
+        // 駅に到着した時に b 人が降りて新たに c 人が乗車する時、
+        // 電車に乗っている乗客人数を求めてください。
+
+        // 半角スペース区切りで文字列型で配列化
+        string[] input = Console.ReadLine().Split(" ");
+
+        // 文字列型の配列を整数型の配列に変換
+        int[] nums = Array.ConvertAll(input, int.Parse);
+
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        // debug
+        foreach (int num in nums)
+        {
+            Console.WriteLine(num);
+        }
+
+        //int result = (a - b - c);
+        //Console.WriteLine(result);
     }
 }
